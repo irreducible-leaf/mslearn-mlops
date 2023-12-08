e4dd74c7de9a9729ff679cd1823ec52282ced452
irreducible-leaf <153296036+irreducible-leaf@users.noreply.github.com>
2023-12-08 15:04:51 +0000

    Convert a notebook to production code
    From challenge 0 described here https://microsoftlearning.github.io/mslearn-mlops/documentation/00-script.html

--- src/model/train.py
+++ src/model/train.py
@@ -4,25 +4,65 @@ import argparse
 import glob
 import os
 
+import numpy as np
 import pandas as pd
+import mlflow
+import mlflow.sklearn
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
+
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
+    # Start Logging
+    mlflow.start_run()
 
+    # enable autologging
+    # mlflow.sklearn.autolog()
+    mlflow.autolog()
+
+    # print training run options and hyperparameters to the console
+    print(" ".join(f"{k}={v}" for k, v in vars(args).items()))
 
     # read data
     df = get_csvs_df(args.training_data)
 
+    mlflow.log_metric("num_samples", df.shape[0])
+    mlflow.log_metric("num_features", df.shape[1] - 1)
+
     # split data
-    X_train, X_test, y_train, y_test = split_data(df)
+    X_train, X_test, y_train, y_test = split_data(df, args.test_train_ratio)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
+    # evaluate model
+    metrics = evaluate_model(model, X_test, y_test)
+    print(" ".join(f"{k}={v}" for k, v in metrics.items()))
+
+
+    # Registering the model to the workspace
+    # print("Registering the model via MLFlow")
+    # mlflow.sklearn.log_model(
+    #     sk_model=model,
+    #     registered_model_name=args.registered_model_name,
+    #     artifact_path=args.registered_model_name,
+    # )
+
+    # Saving the model to a file
+    mlflow.sklearn.save_model(
+        sk_model=model,
+        path=os.path.join(args.registered_model_name, "trained_model"),
+    )
+
+    # Stop Logging
+    mlflow.end_run()
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -34,11 +74,39 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df, test_train_ratio):
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI',
+               'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_train_ratio, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
+    return model
+
+def evaluate_model(model, X_test, y_test):
+    y_hat = model.predict(X_test)
+    acc = np.average(y_hat == y_test)
+
+    y_scores = model.predict_proba(X_test)
+    auc = roc_auc_score(y_test,y_scores[:,1])
+
+    # # plot ROC curve
+    # fpr, tpr, thresholds = roc_curve(y_test, y_scores[:, 1])
+    # fig = plt.figure(figsize=(6, 4))
+    # # Plot the diagonal 50% line
+    # plt.plot([0, 1], [0, 1], 'k--')
+    # # Plot the FPR and TPR achieved by our model
+    # plt.plot(fpr, tpr)
+    # plt.xlabel('False Positive Rate')
+    # plt.ylabel('True Positive Rate')
+    # plt.title('ROC Curve')
+    metrics = {
+        "Accuracy": acc,
+        "ROC AUC": auc
+    }
+    return metrics
 
 
 def parse_args():
@@ -50,12 +118,16 @@ def parse_args():
                         type=str)
     parser.add_argument("--reg_rate", dest='reg_rate',
                         type=float, default=0.01)
+    parser.add_argument("--test_train_ratio", type=float, required=False, default=0.3)
+    parser.add_argument("--registered_model_name", type=str, help="model name")
+
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
